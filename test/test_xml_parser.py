3ee03f01f1324eb1e67b45492b7ed366bd6c0117
Robin Senn <61687542+rssen@users.noreply.github.com>
2020-06-23 19:58:01 +0200

    Add parsing of dishes from XML-API

---
+++ test/test_xml_parser.py
@@ -0,0 +1,11 @@
+import unittest
+from handler import xml_parser
+
+
+class TestGeneratingDishes(unittest.TestCase):
+    def test_xml_parser(self):
+        all_dishes = xml_parser.parse_xml("test_menu.xml")
+
+
+if __name__ == '__main__':
+    unittest.main()
