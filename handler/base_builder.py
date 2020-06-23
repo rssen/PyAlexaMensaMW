093806f92a60cda6a728dc64081ebfeb93bce0b2
Robin Senn <61687542+rssen@users.noreply.github.com>
2020-06-23 20:00:45 +0200

    Add DayIntentHandler
    Ref. #14

---
+++ handler/base_builder.py
@@ -0,0 +1,30 @@
+from ask_sdk_core.skill_builder import SkillBuilder
+from handler.standard_handler import (
+    SessionEndedRequestHandler,
+    ExitIntentHandler,
+    HelpIntentHandler,
+    LaunchRequestHandler,
+    CatchAllExceptionHandler,
+)
+from handler.custom_handler import (
+    DayIntentHandler
+)
+from handler.xml_parser import parse_xml
+
+sb = SkillBuilder()
+sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_request_handler(ExitIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+# custom handler
+dishes = parse_xml("https://app.hs-mittweida.de/speiseplan/all")
+assert len(dishes) > 0
+assert dishes is not None
+a = DayIntentHandler()
+a.dishes = dishes
+sb.add_request_handler(a)
+
+
+handler = sb.lambda_handler()
---
+++ handler/custom_handler.py
@@ -0,0 +1,28 @@
+from typing import List, Sequence
+from datetime import datetime
+
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.handler_input import HandlerInput
+
+from handler.xml_parser import Dish
+from ask_sdk_core.utils import is_intent_name, get_slot_value
+from ask_sdk_core.skill_builder import SkillBuilder
+
+sb = SkillBuilder()
+
+class DayIntentHandler(AbstractRequestHandler):
+
+    dishes: Sequence[Dish]
+
+    def can_handle(self, handler_input: HandlerInput) -> bool:
+        return is_intent_name("DayIntent")(handler_input)
+
+    def handle(self, handler_input: HandlerInput):
+        # get slot value
+        day_slot = get_slot_value(handler_input=handler_input, slot_name="day")
+        print(day_slot)
+
+        day_name = ['montag', 'dienstag', 'mittwoch', 'donnerstag', 'freitag', 'samstag', 'sonntag']
+        day_of_week = datetime.today().weekday()
+        day_slot_number = day_name.index(day_slot)
+
