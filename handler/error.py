85a0c8032ee6dc0240fa9787941e69273596e75e
Robin Senn <61687542+rssen@users.noreply.github.com>
2021-03-06 18:37:50 +0100

    Increase verbosity of SkillException

--- handler/error.py
+++ handler/error.py
@@ -1,3 +1,19 @@
+from enum import Enum
+
+
 class SkillException(Exception):
-    def __init__(self, message):
-        super.__init__(message)
+    def __init__(self, message: str, severity: "Severity", location: "Location"):
+        msg = f"error: severity->{severity.name.lower()} location->{location.name.lower()} msg->{message}"
+        super().__init__(msg)
+
+
+class Severity(Enum):
+    FATAL = 1
+    WARNING = 2
+
+
+class Location(Enum):
+    XML_PARSER = "xml-parser"
+    OUTPUT_TEMPLATE = "template"
+    DAY_INTENT_HANDLER = "day_intent_handler"
+
